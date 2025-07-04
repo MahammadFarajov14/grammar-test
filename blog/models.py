7adee358f37f2850bdce120fcde1dafa9498b179
Mahammad Farajov14 <mahammadfarajov1029876543@gmail.com>
2025-07-04 16:20:01 +0400

    changes

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from core.models import AbstractModel
 from django.contrib.auth import get_user_model
-User = get_user_model()
+CustomUser = get_user_model()
 from core.validators import validate_file_size
 from blog.validators import validate_file_type
 
 # Create your models here.
 
 class Post(AbstractModel):
-    userId = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    userId = models.ForeignKey(CustomUser, related_name='posts', on_delete=models.CASCADE)
 
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
@@ -27,7 +27,7 @@ class Post(AbstractModel):
     
 class Comment(AbstractModel):
     parentCommentId = models.ForeignKey('self', related_name='child', on_delete=models.CASCADE, null=True, blank=True)
-    userId = models.ForeignKey(User, related_name='comment_user', on_delete=models.CASCADE)
+    userId = models.ForeignKey(CustomUser, related_name='comment_user', on_delete=models.CASCADE)
     postId = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE, null=True, blank=True)
 
     like = models.PositiveIntegerField(default=0)
@@ -47,8 +47,8 @@ class Notification(AbstractModel):
         ('comment_like', 'Like comment'),
         ('commented_comment', 'Commented to comment'),
     )
-    recipient = models.ForeignKey(User, related_name='notification', on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, related_name='sent_notification', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(CustomUser, related_name='notification', on_delete=models.CASCADE)
+    sender = models.ForeignKey(CustomUser, related_name='sent_notification', on_delete=models.CASCADE)
     type = models.CharField(max_length=50, choices=NOTIFICATION_TYPES)
     postId = models.ForeignKey(Post, related_name='notification_post', on_delete=models.CASCADE, null=True, blank=True)
     commentId = models.ForeignKey(Comment, related_name='notification_comment', on_delete=models.CASCADE, null=True, blank=True)
